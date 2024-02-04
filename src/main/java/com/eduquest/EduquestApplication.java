6801a7da472fa910c2b6b5022ad84040976efb4b
Osxbor <osxbor123@gmail.com>
2024-02-03 22:19:09 -0600

    first commit

---
+++ src/main/java/com/eduquest/EduquestApplication.java
@@ -0,0 +1,13 @@
+package com.eduquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduquest/EduquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
